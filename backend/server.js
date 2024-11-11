51899a3e72e033ad6a61a83bbdd535260d9ab362
Yogindra <yogindrachaudhari816@gmail.com>
2024-11-12 02:18:49 +0530

    cors added

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,14 @@ const jobRoutes = require("./routes/jobRoutes"); // Import job routes
 require("dotenv").config();
 
 const app = express();
-app.use(cors()); // Allow all CORS requests for now
+// app.use(cors()); // Allow all CORS requests for now
+app.use(
+  cors({
+    origin: ["http://localhost:1505", "https://job-scape.vercel.app"], // Allow local and production URLs
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+    credentials: true, // Allow cookies and other credentials
+  })
+);
 app.use(express.json()); // Middleware to parse JSON requests
 
 // MongoDB connection
